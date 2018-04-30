97bd1a9fe94cbe80378af9ac82a9ccedf043c00e
hk <hkccoin@gmail.com>
2018-04-29 17:34:21 -0700

    second commit

---
+++ sequence_parser.py
@@ -0,0 +1,42 @@
+# Reads GH1 Sequence, counts how many of each nucleotides there are.
+# Calculates the GC content
+# Written by: Hoikin Cheng
+
+nt_counter = {"A" : 0, "T" : 0, "C" : 0, "G" : 0}
+total_nt = 0
+
+with open("GH1Sequence", "r") as sequence:
+    for line in sequence:
+        if line.startswith(">"):
+            print("Sequence title:")
+            print(line.replace(">", ""))
+        else:
+            for each_nt in line:
+                if (each_nt == "A"):
+                    nt_counter["A"] += 1
+                    total_nt += 1
+                elif (each_nt == "T"):
+                    nt_counter["T"] += 1
+                    total_nt += 1
+                elif (each_nt == "C"):
+                    nt_counter["C"] += 1
+                    total_nt += 1
+                elif (each_nt == "G"):
+                    nt_counter["G"] += 1
+                    total_nt += 1
+                else:
+                    continue
+
+print("Total number of A: %i" %nt_counter["A"])
+print("Total number of T: %i" %nt_counter["T"])
+print("Total number of C: %i" %nt_counter["C"])
+print("Total number of G: %i" %nt_counter["G"])
+
+print("\n")
+
+print("Total nt: %i" %total_nt)
+
+print("\n")
+
+print("CG content (percentage): %f"\
+%( (nt_counter["C"] + nt_counter["G"]) * 100 / float (total_nt) ) )
